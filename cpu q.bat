c507a97d06015475a768057dd446b6609a44056c
egorf6 <60822470+egorf6@users.noreply.github.com>
2020-02-08 19:57:32 +0300

    Add files via upload

---
+++ cpu q.bat
@@ -0,0 +1,16 @@
+@echo off
+color 14
+echo created by Egor F.
+echo        _____
+echo       ( . . )
+echo        _____
+echo        !!!!!
+echo        -----
+echo it may take some time
+systeminfo
+echo click 1 to exit
+set /p b=[exit]
+if b=1(
+exit
+  )else(  
+pause
